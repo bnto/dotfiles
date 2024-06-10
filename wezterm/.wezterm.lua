417a97b003b94e70005ec93a9a3e9c831cd6ab93
bnto <bnto@users.noreply.github.com>
2024-06-10 15:43:11 +0200

    Move wezterm config to its own folder and add tmux config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,36 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.default_prog = { "/bin/bash", "-l" }
+config.font_size = 21
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.color_scheme = "Cobalt2"
+
+config.quit_when_all_windows_are_closed = false
+config.window_close_confirmation = "NeverPrompt"
+
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = false
+-- disable ligatures
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+
+-- window size
+config.adjust_window_size_when_changing_font_size = false
+
+config.keys = {
+  -- Turn off the default CMD-m Hide action, allowing CMD-m to
+  -- be potentially recognized and handled by the tab
+  {
+    key = "m",
+    mods = "CMD",
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+}
+
+-- and finally, return the configuration to wezterm
+return config
