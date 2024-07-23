24e9fdd80212a816c7b82f0f95249d29a9c4c5fa
bnto <bnto@users.noreply.github.com>
2024-07-23 15:29:38 +0200

    Use rose-pine-dawn colorscheme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,7 +9,59 @@ config.default_prog = { "/bin/bash", "-l" }
 config.font_size = 18
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
-config.color_scheme = "Cobalt2"
+config.color_scheme = "rose-pine-dawn"
+-- config tabbar color for rose-pine-dawn
+local dawn_palette = {
+  base = "#faf4ed",
+  overlay = "#f2e9e1",
+  muted = "#9893a5",
+  text = "#575279",
+  love = "#b4637a",
+  gold = "#ea9d34",
+  rose = "#d7827e",
+  pine = "#286983",
+  foam = "#56949f",
+  iris = "#907aa9",
+  -- highlight_high = '#cecacd',
+}
+
+local dawn_active_tab = {
+  bg_color = dawn_palette.overlay,
+  fg_color = dawn_palette.text,
+}
+
+local dawn_inactive_tab = {
+  bg_color = dawn_palette.base,
+  fg_color = dawn_palette.muted,
+}
+
+config.colors = {
+  tab_bar = {
+    background = dawn_palette.base,
+    active_tab = {
+      bg_color = dawn_palette.overlay,
+      fg_color = dawn_palette.text,
+      intensity = "Normal",
+    },
+    inactive_tab = {
+      bg_color = dawn_palette.base,
+      fg_color = dawn_palette.muted,
+    },
+    inactive_tab_hover = {
+      bg_color = dawn_palette.overlay,
+      fg_color = dawn_palette.text,
+      italic = true,
+    },
+    new_tab = dawn_inactive_tab,
+    new_tab_hover = {
+      bg_color = dawn_palette.overlay,
+      fg_color = dawn_palette.text,
+      italic = true,
+    },
+    inactive_tab_edge = dawn_palette.muted,
+  },
+}
+
 -- Transparent window
 -- config.window_background_opacity = 0.8
 -- config.macos_window_background_blur = 60
