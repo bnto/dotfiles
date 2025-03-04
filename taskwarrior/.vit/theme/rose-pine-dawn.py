9ff65431bf9dece6f5992f60c62fe08bf5976e75
bnto <bnto@users.noreply.github.com>
2025-03-04 09:36:12 +0100

    Add taskwarrior and vit config files

---
+++ taskwarrior/.vit/theme/rose-pine-dawn.py
@@ -0,0 +1,50 @@
+# rose-pine-dawn.py
+
+# theme = [
+#     ('list-header', 'light cyan', 'black', '', '#56949f', '#faf4ed'),  # Foam on Base
+#     ('list-header-column', 'black', 'light gray', '', '#575279', '#fffaf3'),  # Text on Surface
+#     ('list-header-column-separator', 'black', 'light gray', '', '#575279', '#fffaf3'),  # Text on Surface
+#     ('striped-table-row', 'light gray', 'dark gray', '', '#f2e9e1', '#797593'),  # Overlay on Subtle
+#     ('reveal focus', 'black', 'light cyan', 'standout', '#286983', '#faf4ed'),  # Pine on Base
+#     ('message status', 'light gray', 'dark blue', 'standout', '#faf4ed', '#56949f'),  # Base on Foam
+#     ('message error', 'light gray', 'dark red', 'standout', '#faf4ed', '#b4637a'),  # Base on Love
+#     ('status', 'dark magenta', 'black', '', '#907aa9', '#faf4ed'),  # Iris on Base
+#     ('flash off', 'black', 'black', 'standout', '#575279', '#faf4ed'),  # Text on Base
+#     ('flash on', 'light gray', 'black', 'standout', '#faf4ed', '#575279'),  # Base on Text
+#     ('pop_up', 'light gray', 'black', '', '#faf4ed', '#575279'),  # Base on Text
+#     ('button action', 'light gray', 'light red', '', '#faf4ed', '#d7827e'),  # Base on Rose
+#     ('button cancel', 'black', 'light gray', '', '#575279', '#f2e9e1'),  # Text on Overlay
+# ]
+
+# theme = [
+#     ('list-header', 'light cyan', 'black', '', '#56949f', '#faf4ed'),  # Foam on Base
+#     ('list-header-column', 'black', 'light gray', '', '#d7827e', '#fffaf3'),  # Rose on Surface
+#     ('list-header-column-separator', 'black', 'light gray', '', '#d7827e', '#fffaf3'),  # Rose on Surface
+#     ('striped-table-row', 'light gray', 'dark gray', '', '#f2e9e1', '#ea9d34'),  # Overlay on Gold
+#     ('reveal focus', 'black', 'light cyan', 'standout', '#286983', '#faf4ed'),  # Pine on Base
+#     ('message status', 'light gray', 'dark blue', 'standout', '#faf4ed', '#56949f'),  # Base on Foam
+#     ('message error', 'light gray', 'dark red', 'standout', '#faf4ed', '#b4637a'),  # Base on Love
+#     ('status', 'dark magenta', 'black', '', '#907aa9', '#faf4ed'),  # Iris on Base
+#     ('flash off', 'black', 'black', 'standout', '#575279', '#faf4ed'),  # Text on Base
+#     ('flash on', 'light gray', 'black', 'standout', '#faf4ed', '#575279'),  # Base on Text
+#     ('pop_up', 'light gray', 'black', '', '#faf4ed', '#575279'),  # Base on Text
+#     ('button action', 'light gray', 'light red', '', '#faf4ed', '#d7827e'),  # Base on Rose
+#     ('button cancel', 'black', 'light gray', '', '#575279', '#ea9d34'),  # Text on Gold
+# ]
+
+theme = [
+    ('list-header', 'light cyan', 'black', '', 'light cyan', 'black'),  # Light cyan on black
+    ('list-header-column', 'black', 'light gray', '', 'black', 'light gray'),  # Black on light gray
+    ('list-header-column-separator', 'black', 'light gray', '', 'black', 'light gray'),  # Black on light gray
+    ('striped-table-row', 'light gray', 'dark gray', '', 'light gray', '#faf4ed'),  # Light gray on dark gray
+    ('reveal focus', 'black', 'light cyan', 'standout', 'black', 'light cyan'),  # Black on light cyan
+    # ('message status', 'light gray', 'dark blue', 'standout', 'light gray', 'dark blue'),  # Light gray on dark blue
+    ('message status', 'light gray', 'dark blue', 'standout', '#faf4ed', '#56949f'),  # Base on Foam
+    ('message error', 'light gray', 'dark red', 'standout', 'light gray', 'dark red'),  # Light gray on dark red
+    ('status', 'light magenta', 'black', '', 'light magenta', 'black'),  # Light magenta on black
+    ('flash off', 'black', 'black', 'standout', 'black', 'black'),  # Black on black
+    ('flash on', 'light gray', 'black', 'standout', 'light gray', 'black'),  # Light gray on black
+    ('pop_up', 'light gray', 'black', '', 'light gray', 'black'),  # Light gray on black
+    ('button action', 'light gray', 'light red', '', 'light gray', 'light red'),  # Light gray on light red
+    ('button cancel', 'black', 'light gray', '', 'black', 'light gray'),  # Black on light gray
+]
